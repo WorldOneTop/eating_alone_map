9fdfdf6028751e78ac3c7388d24370f7bd8dc838
dlwpdlf147 <dlwpdlf147@naver.com>
2021-11-04 00:34:02 +0900

    infoHouse(식당 정보 카드) create image onclick, network process

--- lib/layouts/info_house.dart
+++ lib/layouts/info_house.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class InfoHouse {
 
   Container buildLayout(BuildContext context, String title,{String? image,  String category = '?', int? rating, int review=0,double height = 100,String heart=''}) {
     String inputRating = '';
-    image ??= 'assets/images/tempImage.png';
-
+    Image inputImage;
     if(rating != null) {
-      inputRating = '★ $rating/5 ┃';
-    }
-    if(height < 65) {
+      inputRating = '★ $rating/5 ㅣ ';
+    }if(height < 65) {
       height = 65;
     }
-    Image inputImage = Image.asset(image,fit:BoxFit.cover,height: height);
+    if(image == null) {
+      inputImage = Image.asset('assets/images/tempImage.png',fit:BoxFit.cover,height: height);
+    }else{
+      inputImage = Image.asset(image,fit:BoxFit.cover,height: height);
+      //네트워크 이미지 다루기
+    }
+
 //    Container(child:Center(child:inputImage,widthFactor: null,heightFactor: null,),color: Colors.black38,)
     return Container(
       height: height,
       color: const Color(0xFFF5F5F5),
       child: Row(children: [
         Expanded(flex: 1,child: Stack( children: [
-          Center(child:inputImage),
+          GestureDetector(child:Center(child:inputImage), onTap: (){
+            Fluttertoast.showToast(msg: "이미지 새창으로 띄우기 처리");
+          },),
           Positioned( bottom: 2, left: 4,
               child: Text(heart,style:const TextStyle(color: Color(0xF0F86A5B), fontSize: 18)))
         ])),
@@ -27,9 +34,9 @@ class InfoHouse {
             child:Column(children: [
               const SizedBox(height: 5),
               Text(title, maxLines:height > 94 ? 2 : 1, overflow: TextOverflow.ellipsis, textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headline3?.copyWith(height: 1.0)),
+              style: Theme.of(context).textTheme.headline3?.copyWith(height: 1.0),),
               Expanded(child: Container()),
-              Text('$category ┃ $inputRating 리뷰 수 : $review',
+              Text('$category ㅣ $inputRating리뷰 수 : $review',
                   textAlign: TextAlign.center, style: Theme.of(context).textTheme.headline5)
             ])
         )
