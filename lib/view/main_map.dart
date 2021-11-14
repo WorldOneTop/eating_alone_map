d6a69d950386f80336455864f0bd1388c1bf0037
dlwpdlf147 <dlwpdlf147@naver.com>
2021-11-14 17:40:27 +0900

    main->map  and serach page create

--- lib/view/main_map.dart
+++ lib/view/main_map.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
-
+import 'layouts/area_setting.dart';
+import 'layouts/info_house.dart';
 
 
 class MainMap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: const EdgeInsets.fromLTRB(10, 15, 10, 0),
+      child: ListView(children: [
+        Text('주변식당',style: Theme.of(context).textTheme.headline4),
+        Container(
+          height: 350,
+          color: Colors.blueGrey,
+          margin: const EdgeInsets.all(20)
+        ),
+        HouseList()
+      ]),
+    );
+  }
+}
+
+class HouseList extends StatefulWidget {
+  @override
+  _HouseListState createState() => _HouseListState();
+}
+
+class _HouseListState extends State<HouseList> {
+  List<Widget> houseList = [AreaSetting()];
+
+  @override
+  void initState() {
+    for(int i=0;i<10;i++){
+      houseList.add(SizedBox(height: 20,));
+      houseList.add(InfoHouse('강릉 육사시미',category: '한식', rating: 4.5,review: 93,image: 'https://picsum.photos/100'));
+      houseList.add(SizedBox(height: 20,));
+      houseList.add(InfoHouse('24시 전주 명가 콩나물           국밥 강릉점',category: '한식',heart: '♡', rating: 4.3,review: 824,image: 'https://picsum.photos/100' ));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: houseList.length,
+        itemBuilder: (context, index) {
+          return houseList[index];
+        }
+    );
   }
 }
---
+++ lib/view/search_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'layouts/appbar.dart';
+import 'package:eating_alone/model/enum.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  TextEditingController controller = TextEditingController();
+  List<String> default_wordList = ['자동완성 되어 보여질 식당 이름들 리스트','enum class에 저장해서 onchange event로 list update','아무 것도 입력 안했을 때는',
+      '로그 관리해서 최근 검색 기록 순서대로','보여주기',
+    '테스트 워드','가나다라마바사','가나다라마바','가나다라','가마바','나라마바','가나다라 가나다라 가나다라 가나다라'];
+  List<RichText>? input_wordList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: CustomAppbar.getInstance().getAppBar(context, Appbar_mode.search, '검색어를 입력하세요.',ctr: controller,
+            onChange: (text){setState(() {
+            if(text.isEmpty) {
+              input_wordList = null;
+            }else {
+              setInputWordList(text);
+            }});
+        }),
+        body:ListView.separated(
+            padding: const EdgeInsets.fromLTRB(15, 20, 15, 0),
+            itemBuilder: wordBuilder,
+            separatorBuilder: (BuildContext context, int index) {
+              return Divider();
+            },
+            itemCount: (input_wordList ?? default_wordList).length
+        ));
+  }
+  Widget wordBuilder(BuildContext context,int index) {
+    if(controller.text.isEmpty) {
+      return GestureDetector(
+        onTap: (){
+          controller.text = default_wordList[index];
+          setInputWordList(controller.text);
+          setState(() {
+          });
+        },
+        child: Text(default_wordList[index],overflow: TextOverflow.ellipsis,style: const TextStyle(fontSize: 18),));
+    }else {
+      return GestureDetector(
+          onTap: (){
+            controller.text = input_wordList![index].text.toPlainText();
+            setInputWordList(controller.text);
+            setState(() {
+            });
+          },
+          child: input_wordList![index]
+    );}
+  }
+  void setInputWordList(String text){
+    input_wordList = [];
+    for(String str in default_wordList) {
+      List<String> strSplit = str.split(text);
+      if(strSplit.length != 1) { // 일치문자열 있을때
+        List<TextSpan> inputText = []; // 강조할 텍스트 구분
+        TextStyle boldTextStyle = const TextStyle(fontWeight: FontWeight.bold);
+
+        if(strSplit[0].isEmpty) {
+          inputText.add(TextSpan(text: text,style: boldTextStyle));
+        }
+        for(int i=0;i<strSplit.length-1;i++) {
+          if(strSplit[i].isNotEmpty){
+            inputText.add(TextSpan(text: strSplit[i]));
+            inputText.add(TextSpan(text: text,style: boldTextStyle));
+          }
+        }
+        if(strSplit.last.isNotEmpty) {
+          inputText.add(TextSpan(text: strSplit.last));
+        }
+        input_wordList!.add(RichText(
+          text:TextSpan(
+            style: const TextStyle(fontSize: 18,color: Colors.black,fontFamily:'Elice' ),
+            children: inputText)));
+      }
+    }
+  }
+}
