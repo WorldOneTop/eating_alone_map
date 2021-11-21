7a041bb1297b01322955d67d52f66566457bb662
dlwpdlf147 <dlwpdlf147@naver.com>
2021-11-21 23:28:33 +0900

    append test data list

--- lib/model/enum.dart
+++ lib/model/enum.dart
@@ -1,7 +1,7 @@
 class DataList{
   static final menuName = ['한식','분식','카페,디저트','일식','치킨','피자','아시안,양식','중식','도시락','패스트푸드','기타'];
   static final List<String> areaList = ['서울', '인천', '수원', '대전', '대구', '광주', '부산'];
-
+  static final List<String> questionCategory = ['계정','이용문의','불편사항','정보등록','기타'];
 }
 
 enum Appbar_mode {main, search, detail,menu}
